c2e2d504d6ab2693e542b4cdd7bdc3b1b331215c
ahm3dElgharbawy <ahm3delsafy@gmail.com>
2023-09-04 19:29:55 +0200

    using cached network image

--- lib/features/home/presentation/view/widgets/featured_books/featured_books_list_view_item.dart
+++ lib/features/home/presentation/view/widgets/featured_books/featured_books_list_view_item.dart
@@ -1,6 +1,6 @@
 import 'dart:ui';
 
-import 'package:bookly_app/constants.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
 class FeaturedBooksListViewItem extends StatelessWidget {
@@ -11,33 +11,37 @@ class FeaturedBooksListViewItem extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.only(right: 16),
-      child: AspectRatio(
-        aspectRatio: 2 / 3,
-        child: Container(
-          decoration: kBoxDecorationWithRadius20.copyWith(
-            image: DecorationImage(
-              image: NetworkImage(imageUrl),
-              fit: BoxFit.cover,
-            ),
-          ),
-          child: Align(
-            alignment: Alignment.bottomRight,
-            child: Padding(
-              padding: const EdgeInsets.all(10),
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(20),
-                child: BackdropFilter(
-                  filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
-                  child: const SizedBox(
-                    height: 40,
-                    width: 40,
-                    child: Icon(
-                      Icons.play_arrow_rounded,
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(15),
+        child: AspectRatio(
+          aspectRatio: 2 / 3,
+          child: Stack(
+            fit: StackFit.expand,
+            children: [
+              CachedNetworkImage(
+                imageUrl: imageUrl,
+                fit: BoxFit.cover,
+              ),
+              Align(
+                alignment: Alignment.bottomRight,
+                child: Padding(
+                  padding: const EdgeInsets.all(10),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(20),
+                    child: BackdropFilter(
+                      filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+                      child: const SizedBox(
+                        height: 40,
+                        width: 40,
+                        child: Icon(
+                          Icons.play_arrow_rounded,
+                        ),
+                      ),
                     ),
                   ),
                 ),
               ),
-            ),
+            ],
           ),
         ),
       ),
